e694a128efacd1bffc795e3a285eb824996b518e
Jacob Libby (he/him) <69590611+JacobLibby@users.noreply.github.com>
2022-05-02 23:24:35 -0400

    Committing changes

--- js/Script.js
+++ js/Script.js
@@ -1,3 +1,54 @@
+/*
+GENERAL TODO:
+JACOB:
+  Update user fields
+    bio, full_name, etc
+
+  Update login status
+    When user is registered, does it automatically run login func?
+    TODO: Check if register will update online val
+  
+  Create Group chat
+    ? Add to user-side groups array & to group-side
+
+  Delete Chat - Make an admin of group?
+    Onclick, set currentGroupId, so that when delete button pressed, it deletes currently opened chat?
+    Update sidebar
+    ? Permissions?
+
+  Delete Messages - Only delete own + admin privileges?
+    Onclick next to message
+    Delete from group-side
+    Update sidebar
+    ? Permissions?
+
+  Remove user from chat - Self vs admin
+    Remove from user-side groups array & from group-side members
+    Update sidebar
+    ? Delete messages from deleted users?
+
+  Populate db
+    Last-ish step
+
+  Password Reset
+    Watch tutorial --> current breaks once sendReset...... func called
+
+
+
+OTHERS:
+  Search for other users
+
+  Profile page/sidebar
+    
+
+
+where to input certain user profile fields (bio, full name, etc)
+Search for other users? - primary field? (full name, email, ID?) --> If ID, make visible on profile page
+
+Pull all groups user is a member of for sidebar nav --> handle case if none, too many, maybe sort by last login of users? last message?
+*/
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCPMUGPrljaQhxxnV8cij4FaKTk1CQKYzY",
     authDomain: "jaichat-1c155.firebaseapp.com",
@@ -11,6 +62,8 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 const auth = firebase.auth();
+// TODO: create group chat functionality --> update currentGroupID when selecting different group chats. Var passed to fetch and post functions
+var currentGroupID = '';
 
 firebase.auth().onAuthStateChanged(function(user){
   if(user){
@@ -19,7 +72,7 @@ firebase.auth().onAuthStateChanged(function(user){
     document.getElementById("container").style.display = "none";
     document.getElementById("container_createAccount").style.display = "none";
 
-    var user = auth.currentUser;
+    var user = auth.currentUser; // TODO: what is stored here?
 
     if(user != null){
       var email = user.email;
@@ -48,33 +101,117 @@ function tapped_alreadyHaveAccount(){
   document.getElementById("container_createAccount").style.display = "none";
 };
 
-function register () {
-  email = document.getElementById('new_email').value
-  password = document.getElementById('new_password').value
+// TODO: Forgot Password
 
+function forgot_password(){
+//import { getAuth, sendPasswordResetEmail } from "firebase/auth"; // Needed?
+  // alert('FORGOT PASSWORD')
+  email = document.getElementById('email').value;
+  //const auth = getAuth(); // Needed?
+  //sendPasswordResetEmail(auth, email) // user, email ??
+  sendPasswordResetEmail(email, )
+    .then(() => {
+      // Password reset email sent!
+      alert('Password reset email sent');
+    })
+    .catch((error) => {
+      alert('Error occured while attempting to send password reset email');
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+    alert('FORGOT PASSWORD');
+  return
+}
+
+
+function setUserData () { // TODO: Should this function set new username, email, password?
+  /*
+
+  var user_data = {
+    online: true
+  }
+  if (document.getElementById('new_bio').value) {
+    bio = document.getElementById('new_bio').value;
+    user_data['bio'] = bio;
+  }
+
+
+  if (document.getElementById('new_password').value){
+    password = document.getElementById('new_password').value;
+    // User firebase to reset password
+  }
+
+  if (document.getElementById('new_full_name').value){
+    full_name = document.getElementById('new_full_name').value;    
+    user_data['full_name'] = full_name;
+  }
+
+  if (document.getElementById('new_email').value){
+    full_name = document.getElementById('new_email').value;    
+    // User firebase to reset email // <-- is this possible?
+  }
+
+  database_ref.child('users/' + user.uid).set(user_data);
+
+
+  */
+
+}; // TODO: When do you add this?
+
+
+function register () { // TODO: How does registering an account automatically log-in the user and show chat?
+  // TODO: get value of username
+  username = document.getElementById('username').value;
+  email = document.getElementById('new_email').value;
+  password = document.getElementById('new_password').value;
+  // TODO: check if passwords match (currently only uses one instance of password)
+  password2 = document.getElementById('confirm_password').value;
+
+
+  if (password != password2) {
+    alert('Passwords do not match')
+    return
+  }
   if (validate_email(email) == false ) {
     alert('Email is not valid')
     return 
   }
   if(validate_password(password) == false){
-    alert('password is not valid')
+    alert('Password is not valid')
     return 
   }
+
+  
 
   auth.createUserWithEmailAndPassword(email, password)
   .then(function() {
     var user = auth.currentUser
 
     var database_ref = database.ref()
+    var group_array = new Array('everyone'); // create an array of groups, where all users are a part of "everyone" group
 
     var user_data = {
+      username: username,
       email : email, 
-      last_login : Date.now()
+      last_login : Date.now(),
+      bio: "",
+      online: true,
+      groups: group_array,
+      fullname: ""
+      // TODO: add other data (username)
     }
 
     database_ref.child('users/' + user.uid).set(user_data)
 
     alert('User Created!!')
+
+
+    // TODO: clear input fields
+    document.getElementById('username').value         = '';
+    document.getElementById('new_email').value        = '';
+    document.getElementById('new_password').value     = '';
+    document.getElementById('confirm_password').value = '';
+    
     //location.href = 'index.html'
   })
   .catch(function(error) {
@@ -85,9 +222,11 @@ function register () {
   })
 };
 
-function login () {
-  var email = document.getElementById('email').value
-  var password = document.getElementById('password').value
+function login () { // TODO: register does not execute "log in" func
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+ 
 
   if (validate_email(email) == false || validate_password(password) == false) {
     alert('Email or Password are incorrect')
@@ -101,13 +240,20 @@ function login () {
     var database_ref = database.ref()
 
     var user_data = {
-      last_login : Date.now()
+      last_login : Date.now(),
+      online: true
     }
 
     database_ref.child('users/' + user.uid).update(user_data)
 
-    alert('User Logged In!!')
+    // TODO: clear input fields
+    document.getElementById('email').value = '';
+    document.getElementById('password').value = '';
 
+
+    alert('User Logged In!!')
+    
+    // TODO: send to blank page with group chats on left side?
 
   })
   .catch(function(error) {
@@ -116,11 +262,22 @@ function login () {
 
     alert(error_message)
   })
-}
+};
 
-function logOut() {
+function logOut() { // TODO: currently, when user logs out, or screen changes from messaging, message that is typed but not sent is NOT DELETED (visible across logins)
+  var user = auth.currentUser
+
+  var database_ref = database.ref()
+
   auth.signOut().then(() => {
     alert('User Logged Out!!')
+
+    var user_data = {
+      online: false
+    }
+    database_ref.child('users/' + user.uid).update(user_data);
+    // TODO: clear currentGroupID
+    currentGroupID = '';
   }).catch((error) => {
     var error_code = error.code
     var error_message = error.message
@@ -128,8 +285,9 @@ function logOut() {
   });
 }
 
-document.getElementById("send-message").addEventListener("submit", postChat);
-function postChat(e) {
+/*
+document.getElementById("send-message").addEventListener("submit", postChat); // 
+function postChat(e) { // TODO: pass currentGroupID to post in correct group
   e.preventDefault();
   var user = auth.currentUser;
   var email = user.email;
@@ -141,15 +299,111 @@ function postChat(e) {
     user: email,
     msg: message,
   });
+} */
+
+
+document.getElementById("send-message").addEventListener("submit", postChat); // 
+function postChat(e) { // TODO: pass currentGroupID to post in correct group
+  var fetchChat_directory = "groups/" + currentGroupID + "/messages/"
+  e.preventDefault();
+  var user = auth.currentUser;
+  var email = user.email;
+  const timestamp = Date.now();
+  const chatTxt = document.getElementById("chat-txt");
+  const message = chatTxt.value;
+  chatTxt.value = "";
+  database.ref(fetchChat_directory + timestamp).set({
+    sender_id: user.uid,
+    msg: message,
+    time_sent: timestamp
+    // seen_by: //empty array?
+  });
 }
 
-const fetchChat = database.ref("messages/");
-fetchChat.on("child_added", function (snapshot) {
+
+/*
+const fetchChat = database.ref("messages/"); // groups/______/messages/
+fetchChat.on("child_added", function (snapshot) { // TODO: pass currentGroupID to fetch correct group
+  const messages = snapshot.val();
+  const msg = "<li>" + messages.user + " : " + messages.msg + "</li>";
+  document.getElementById("messages").innerHTML += msg;
+}); */
+
+
+var fetchChat_directory = "groups/" + currentGroupID + "/messages/" // TODO: UPDATE seen_by
+var fetchChat = database.ref(fetchChat_directory); // groups/______/messages/
+fetchChat.on("child_added", function (snapshot) { // TODO: pass currentGroupID to fetch correct group
   const messages = snapshot.val();
   const msg = "<li>" + messages.user + " : " + messages.msg + "</li>";
   document.getElementById("messages").innerHTML += msg;
 });
 
+function delete_message() { // deletes individual message from database
+// TODO: permissions?
+  // get focused message
+}
+
+function delete_group() { // completely deletes the groupchat
+// TODO: admin permissions?
+  // get focused group
+
+
+}
+
+function create_group() { // create a new group
+// self id is set as admin?
+
+
+// get all users to be added to group?
+  var user = auth.currentUser
+
+  var database_ref = database.ref()
+  var members_array = new Array(user.uid); // create an array of groups, where all users are a part of "everyone" group
+  var messages_array = new Array();
+  messages_array[0] = new Map();
+  messages_array[0]['time_sent'] = Date.now();
+  messages_array[0]['message_body'] = "Welcome to your new group chat :)";
+  messages_array[0]['sender_id'] = user.uid;
+  messages_array[0]['seen_by'] = user.uid;
+  var group_data = {
+    admin: user.uid,
+    members: members_array,
+    messages: messages_array,
+    group_name: user.username + "'s group"
+  }
+
+  database_ref.child('groups/' + user.uid + Date.now).set(group_data)
+
+  alert('Group Created!!')
+}
+
+function add_user_to_group() { // anyone can do
+//// find user to add
+
+// get userId from selected element
+
+//// set group
+
+// dropdown?
+
+
+//// edit group table
+
+// add user id to array of users in group --> edit name?
+
+//// edit new user table 
+
+// add groupid to array of groups in user
+
+
+}
+
+function remove_user_from_group() { // admin only?
+// find user to remove
+// set group
+// edit group table
+// edit removed user table
+}
 
 function validate_email(email) {
   expression = /^[^@]+@\w+(\.\w+)+\w$/
@@ -160,10 +414,11 @@ function validate_email(email) {
   }
 }
 
-function validate_password(password) {
-  if (password < 6) {
+function validate_password(password) { //TODO: update password specifics
+  if (password < 6) { // TODO: Should we check other password specifics?
     return false
   } else {
+    // TODO: Add more cases of valid passwords?
     return true
   }
 }
